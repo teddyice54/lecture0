750c5dfba2ec45b24c5fe914acdea71874bc59a2
teddyice54 <40359797+teddyice54@users.noreply.github.com>
2019-07-15 22:48:22 -0500

    Create syntax.js

---
+++ syntax.js
@@ -0,0 +1,8 @@
+const firstName="Ted";
+const lastName='Koller';
+const arr = ['teaching', 42, true, function() { console.log('hi')}];
+
+//hi I'm a comment
+for (let i=0; i < arr.length; i++) {
+console.log(arr[i]);
+}
