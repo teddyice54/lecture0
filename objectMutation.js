fba6554a5ee5c8f7ac9d084e85eb3e257ff9ff7d
teddyice54 <40359797+teddyice54@users.noreply.github.com>
2019-07-15 23:53:47 -0500

    Update objectMutation.js

--- objectMutation.js
+++ objectMutation.js
@@ -1,16 +1,34 @@
-const o = {
+var o = {
   a: 'a',
   b: 'b',
 obj: {
   key: 'key',
       },
 }
-
-const o2 = Object.assign({}, o)
-
-o2.obj.key = 'new value'
-console.log(o.obj.key)
+var o2 = Object.assign({}, o)
 
 //o2.a = 'new value'
 
 //console.log(o.a)
+
+
+//deep copy
+function deepCopy(obj) {
+  //check if vals are objects
+  // if so copy that object (deep copy)
+  //else return the value
+ var keys = Object.keys(obj)
+  
+var newObject = {}
+  
+  for (let i = 0; i < keys.length; i++) {
+    var key = keys[i]
+if (typeof obj [key] === 'object') {
+  newObject[key] = deepCopy(obj[key])
+  }
+    else {
+      newObject[key] = obj[key]
+    }
+  }
+return Object.assign({}, obj)
+}
