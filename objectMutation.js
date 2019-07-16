13fce790fe3978664864157d1d0d3829ee879553
teddyice54 <40359797+teddyice54@users.noreply.github.com>
2019-07-15 23:34:48 -0500

    Update objectMutation.js

--- objectMutation.js
+++ objectMutation.js
@@ -1,10 +1,16 @@
 const o = {
   a: 'a',
   b: 'b',
+obj: {
+  key: 'key',
+      },
 }
 
-
 const o2 = Object.assign({}, o)
-o2.a = 'new value'
 
-console.log(o.a)
+o2.obj.key = 'new value'
+console.log(o.obj.key)
+
+//o2.a = 'new value'
+
+//console.log(o.a)
